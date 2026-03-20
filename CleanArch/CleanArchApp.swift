8c33fbbd23d7624864211a7172c3457bddb20aa2
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2026-03-20 17:00:44 +0530

    Initial Commit

---
+++ CleanArch/CleanArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchApp.swift
+//  CleanArch
+//
+//  Created by G Yacobu on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
